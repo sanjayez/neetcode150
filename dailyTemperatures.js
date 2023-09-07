cba291639e111f049d3f8c15d703c146a0cea5f6
Sanjay S <sanu2336@gmail.com>
2023-09-07 12:11:31 +0530

    Update dailyTemperatures.js

--- dailyTemperatures.js
+++ dailyTemperatures.js
@@ -50,3 +50,22 @@ const dailyTemperatures = (temperatures) => {
   }
   return ans;
 };
+
+// Easier to read version
+var top = function(a) {
+    return a[a.length - 1];
+}
+
+var dailyTemperatures = function(temperatures) {
+    let stack = [], ans = Array(temperatures.length).fill(0);
+
+    for(let i = 0; i < temperatures.length; i++) {
+        while(stack.length && temperatures[top(stack)] < temperatures[i]) {
+            let index = stack.pop();
+            ans[index] = i - index;
+        }
+        stack.push(i);
+    }
+
+    return ans;
+};
